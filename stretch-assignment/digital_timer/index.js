a228c11b25b96932cd3856a781de4461ee4eb1bb
May Ng <mngmay@gmail.com>
2019-07-08 14:32:48 -0700

    Start & Reset button functionality work on the timer

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -9,28 +9,30 @@ let msTensCount = 0;
 let msHundredsCount = 0;
 let secondOnesCount = 0;
 let secondTensCount = 0;
+let flag = false;
 
 function timer() {
   msTens.textContent = msTensCount;
   msHundreds.textContent = msHundredsCount;
   secondOnes.textContent = secondOnesCount;
   secondTens.textContent = secondTensCount;
-
-  if (msTensCount < 9) {
-    msTensCount++;
-    if (msTensCount === 9) {
-      msTensCount = 0;
-      if (msHundredsCount < 9) {
-        msHundredsCount++;
-        if (msHundredsCount === 9) {
-          msHundredsCount = 0;
-          if (secondOnesCount < 9) {
-            secondOnesCount++;
-            if (secondOnesCount === 9) {
-              secondOnesCount = 0;
-              secondTensCount++;
-              if ((digits.textContent = "10:00")) {
-                digits.classList.add("redDigit", "digit");
+  if (flag) {
+    if (msTensCount < 9) {
+      msTensCount++;
+      if (msTensCount === 9) {
+        msTensCount = 0;
+        if (msHundredsCount < 9) {
+          msHundredsCount++;
+          if (msHundredsCount === 9) {
+            msHundredsCount = 0;
+            if (secondOnesCount < 9) {
+              secondOnesCount++;
+              if (secondOnesCount === 9) {
+                secondOnesCount = 0;
+                secondTensCount++;
+                if ((digits.textContent = "10:00")) {
+                  digits.classList.add("redDigit", "digit");
+                }
               }
             }
           }
@@ -38,6 +40,22 @@ function timer() {
       }
     }
   }
+}
+
+//Event Listeners
+
+document.querySelector(".start").addEventListener("click", flagIt);
+document.querySelector(".reset").addEventListener("click", reset);
+
+function flagIt() {
+  flag = true;
+}
+
+function reset() {
+  msTensCount = 0;
+  msHundredsCount = 0;
+  secondOnesCount = 0;
+  secondTensCount = 0;
 }
 
 //If you wanted to create buttons from JS only
