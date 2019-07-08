304314c9eb080b012f4b4021308c64145c10c0ab
May Ng <mngmay@gmail.com>
2019-07-08 13:47:33 -0700

    WIP - all timer fields count properly. Not set up to stop at 10 seconds yet.

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -3,13 +3,18 @@ const secondOnes = document.querySelector("#secondOnes");
 const msHundreds = document.querySelector("#msHundreds");
 const msTens = document.querySelector("#msTens");
 
-window.setInterval(timer, 100);
+window.setInterval(timer, 10);
 let msTensCount = 0;
 let msHundredsCount = 0;
+let secondOnesCount = 0;
+let secondTensCount = 0;
 
 function timer() {
   msTens.textContent = msTensCount;
   msHundreds.textContent = msHundredsCount;
+  secondOnes.textContent = secondOnesCount;
+  secondTens.textContent = secondTensCount;
+
   if (msTensCount < 9) {
     msTensCount++;
     if (msTensCount === 9) {
@@ -18,9 +23,15 @@ function timer() {
         msHundredsCount++;
         if (msHundredsCount === 9) {
           msHundredsCount = 0;
+          if (secondOnesCount < 9) {
+            secondOnesCount++;
+            if (secondOnesCount === 9) {
+              secondOnesCount = 0;
+              secondTensCount++;
+            }
+          }
         }
       }
     }
-    console.log(msTensCount);
   }
 }
