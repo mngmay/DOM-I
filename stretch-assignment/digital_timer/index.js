037255f238ab8b17df0e85e61c1744b9e9c122a1
May Ng <mngmay@gmail.com>
2019-07-08 12:42:24 -0700

    Created variables for timer

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,6 @@
+const secondTens = document.querySelector("#secondTens");
+const secondOnes = document.querySelector("#secondOnes");
+const msHundreds = document.querySelector("#msHundreds");
+const msTens = document.querySelector("#msTens");
+
+function timer() {}
