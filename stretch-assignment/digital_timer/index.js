69fdd6c029796ca903984e78ba5b0af42be6f6ef
May Ng <mngmay@gmail.com>
2019-07-08 13:36:26 -0700

    WIP - msTens counts to 9

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -3,4 +3,14 @@ const secondOnes = document.querySelector("#secondOnes");
 const msHundreds = document.querySelector("#msHundreds");
 const msTens = document.querySelector("#msTens");
 
-function timer() {}
+window.setInterval(timer, 10);
+let msTensCount = 0;
+let msHundredsCount = 0;
+
+function timer() {
+  msTens.textContent = msTensCount;
+  msHundreds.textContent = msHundredsCount;
+  if (msTensCount < 9) {
+    msTensCount += 1;
+  }
+}
